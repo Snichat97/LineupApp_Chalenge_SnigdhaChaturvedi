348c39d07b709b0d9f9c86ac5ccc805c570904eb
Snichat97 <34672064+Snichat97@users.noreply.github.com>
2022-12-09 11:17:07 -0800

    Update application.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,89 +5,71 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import './App.css'
 
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+    arraycomponents:[]
+    }
+  }
 
   eventLogger = (e, data) => {
     console.log('Event: ', e);
     console.log('Data: ', data);
   };
 
+  addShape = (e,image) =>{
+    e.preventDefault();
+    var newArray = this.state.arraycomponents
+    newArray.push(image)
+    this.setState({arraycomponents:newArray})
+  }
+
+
   render() {
     return (
-      <>
+      <div id="container">
       <Container>
       <Row>
         <Col lg={3}>
           <div style={{paddingLeft:"2rem"}}>
         <h2>Components</h2>
+        <h5>Click on the Component to make it appear on the canvas</h5>
         </div>
-        <Draggable
-        handle=".handle"
-        defaultPosition={{x: 0, y: 0}}
-        position={null}
-        grid={[25, 25]}
-        scale={1}
-        onStart={this.handleStart}
-        onDrag={this.handleDrag}
-        onStop={this.handleStop}>
-        <div>
-          <div className="handle"><img src="square.png"></img></div>
-        </div>
-      </Draggable>
-
-      <Draggable
-        handle=".handle"
-        defaultPosition={{x: 0, y: 0}}
-        position={null}
-        grid={[25, 25]}
-        scale={1}
-        onStart={this.handleStart}
-        onDrag={this.handleDrag}
-        onStop={this.handleStop}>
-        <div>
-          <div className="handle"><img src="circle.png"></img></div>
-        </div>
-      </Draggable>
-        
+        <img src="circle.png" onClick={(e)=>this.addShape(e,"circle.png")}></img>
+        <br></br>
+        <img src="square.png" onClick={(e)=>this.addShape(e,"square.png")}></img>
         </Col>
         <Col>
         <h2>Canvas</h2>
-        <Card style={{align:"center",width:"50rem",height:"20rem","z-index": "-1"}}>
-        <img src="dotted.jpeg"></img>
-        </Card>
+        <img src="dotted.jpeg" style={{align:"center",width:"60rem",height:"40rem","z-index": "-1"}}>
+        </img>
+        <div class="parent">
+        {this.state.arraycomponents.map((image)=>{
+        return(
+          <Draggable
+        style={{position:"fixed"}}
+        handle=".handle"
+        position={null}
+        grid={[25, 25]}
+        scale={1}
+        onStart={this.handleStart}
+        onDrag={this.handleDrag}
+        onStop={this.handleStop}>
+        <div>
+          <div className="image1 handle"><img src={image} className="image1"></img></div>
+        </div>
+      </Draggable>)})}
+      </div>
         </Col>
       </Row>
       </Container>
-      </>
+      </div>
     );
   }
 }
-// function App() {
-  // const [offsetTop, setOffsetTop] = React.useState(0);
-  // React.useEffect(() => {
-  // }, []);
-
-  // const [{ opacity }, dragRef] = useDrag(
-  //   () => ({
-  //     type: ItemTypes.CARD,
-  //     item: { text },
-  //     collect: (monitor) => ({
-  //       opacity: monitor.isDragging() ? 0.5 : 1
-  //     })
-  //   }),
-  //   []
-  // )
-
-  // return (
-  //   <div>
-  //     <div ref={dragRef} style={{ opacity }}>
-  //   {text}
-  // </div>
-
-  //   </div>
-  // );
-// }
 
 export default App;
